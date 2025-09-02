18e92f83b379caa590b84e5ecc9b3ae0c7abbc6c
Ram Gavhane <ramgavhane28@gmail.com>
2025-09-03 00:19:30 +0530

    schema update

---
+++ prisma/migrations/20250902184138_init/migration.sql
@@ -0,0 +1,183 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'STUDENT', 'TNP_OFFICER');
+
+-- CreateEnum
+CREATE TYPE "public"."ApplicationStatus" AS ENUM ('PENDING', 'SHORTLISTED', 'SELECTED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'STUDENT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."StudentProfile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "middleName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "dob" TIMESTAMP(3),
+    "branch" TEXT NOT NULL,
+    "enrollmentYear" INTEGER NOT NULL,
+    "passingYear" INTEGER,
+    "cgpa" DOUBLE PRECISION NOT NULL,
+    "tenthPercent" DOUBLE PRECISION,
+    "tenthYear" INTEGER,
+    "twelfthPercent" DOUBLE PRECISION,
+    "twelfthYear" INTEGER,
+    "diplomaPercent" DOUBLE PRECISION,
+    "diplomaYear" INTEGER,
+    "backlogs" INTEGER NOT NULL,
+    "skills" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StudentProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "techStack" TEXT[],
+    "link" TEXT,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Internship" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "duration" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Internship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Certification" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "organization" TEXT NOT NULL,
+    "issueDate" TIMESTAMP(3),
+    "expiryDate" TIMESTAMP(3),
+    "credentialId" TEXT,
+    "credentialUrl" TEXT,
+
+    CONSTRAINT "Certification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."SocialMediaProfile" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "platform" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "SocialMediaProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Achievement" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "date" TIMESTAMP(3),
+    "type" TEXT,
+
+    CONSTRAINT "Achievement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."JobPost" (
+    "id" TEXT NOT NULL,
+    "postedById" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "companyInfo" TEXT,
+    "description" TEXT NOT NULL,
+    "ctc" TEXT,
+    "deadline" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "JobPost_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."EligibilityCriteria" (
+    "id" TEXT NOT NULL,
+    "jobPostId" TEXT NOT NULL,
+    "minCGPA" DOUBLE PRECISION,
+    "allowedBranches" TEXT[],
+    "maxBacklogs" INTEGER,
+    "minTenth" DOUBLE PRECISION,
+    "minTwelfth" DOUBLE PRECISION,
+    "minDiploma" DOUBLE PRECISION,
+    "passingYear" INTEGER,
+
+    CONSTRAINT "EligibilityCriteria_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Application" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "jobPostId" TEXT NOT NULL,
+    "status" "public"."ApplicationStatus" NOT NULL DEFAULT 'PENDING',
+    "appliedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StudentProfile_userId_key" ON "public"."StudentProfile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EligibilityCriteria_jobPostId_key" ON "public"."EligibilityCriteria"("jobPostId");
+
+-- AddForeignKey
+ALTER TABLE "public"."StudentProfile" ADD CONSTRAINT "StudentProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Project" ADD CONSTRAINT "Project_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Internship" ADD CONSTRAINT "Internship_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Certification" ADD CONSTRAINT "Certification_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."SocialMediaProfile" ADD CONSTRAINT "SocialMediaProfile_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Achievement" ADD CONSTRAINT "Achievement_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."JobPost" ADD CONSTRAINT "JobPost_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."EligibilityCriteria" ADD CONSTRAINT "EligibilityCriteria_jobPostId_fkey" FOREIGN KEY ("jobPostId") REFERENCES "public"."JobPost"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Application" ADD CONSTRAINT "Application_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "public"."StudentProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Application" ADD CONSTRAINT "Application_jobPostId_fkey" FOREIGN KEY ("jobPostId") REFERENCES "public"."JobPost"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
